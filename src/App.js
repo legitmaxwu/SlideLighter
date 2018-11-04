47da56a774afaa5d73b4f5c674a73004a492e4e4
legitmaxwu <chengmaxwu@gmail.com>
2018-11-04 03:56:51 -0800

    frontend is basically done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import objectAssign from 'object-assign';
 import RegionSelect from 'react-region-select';
+import ReactTable from "react-table";
+import 'react-table/react-table.css'
 import './App.css';
-
 import axios from 'axios';
+import Tabs from './tabs';
+import { strict } from 'assert';
 
 const serverURL = 'http://10.142.173.86:5000'
 
@@ -12,100 +13,150 @@ const http = axios.create({
   baseURL: serverURL,
 });
 
-// const language = require('@google-cloud/language');
-
-// const client = new language.v1beta2.LanguageServiceClient({
-//   // optional auth parameters.
-// });
-
-// const document = {};
-// const features = {};
-// const request = {
-//   document: document,
-//   features: features,
-// };
-// client.annotateText(request)
-//   .then(responses => {
-//     const response = responses[0];
-//     // doThingsWith(response)
-//   })
-//   .catch(err => {
-//     console.error(err);
-//   });
-
 class App extends Component {
   constructor (props) {
-		super(props);
-		this.regionRenderer = this.regionRenderer.bind(this);
-		this.onChange = this.onChange.bind(this);
-		this.state = {
-			regions: []
+    super(props);
+    this.regionRenderer = this.regionRenderer.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.state = {
+      regions: [],
+      imgArray: [],
+      slideNum: 0,
+      data: [],
     };
-	}
-	onChange (regions) {
-		this.setState({
+  }
+
+  onChange (regions) {
+    this.setState({
       regions: regions
     });
-	}
-	regionRenderer (regionProps) {
-		return (
+  }
+
+  regionRenderer (regionProps) {
+    return (
       <div style={{ position: 'absolute', left: 0,
       pointerEvents: 'none',
-      height: this.state.regions[regionProps.index].height * 3.84, 
-      width: this.state.regions[regionProps.index].width * 5.12, 
-      backgroundColor: 'rgba(255, 255, 0, 0.5)'}}>
-      </div>
-		);
+      height: this.state.regions[regionProps.index].height * 7.68, 
+      width: this.state.regions[regionProps.index].width * 10.24, 
+      backgroundColor: 'rgba(255, 255, 0, 0.5)'}}/>
+    );
   }
-  // getdict = async () => {
-  //   http.post('/getdict', { // FIX DIS SHITE
-  //   }) 
-  //   .then((result) => {
-  //     console.log(result)
-  //   }).catch(err=>console.log(err))
-  // }
+
   uploadCoords = async () => {
     const {regions} = this.state;
     for (let i = 0; i < regions.length; i++) {
-      document.write('(' + regions[i].x + ' ' + regions[i].y + ' ' + (100 - regions[i].x - regions[i].width) + ' ' + (100 - regions[i].y - regions[i].height) + ')')
-      document.write("\n");
       http.post('/savepoints', { // FIX DIS SHITE
         x1: regions[i].x,
         x2: regions[i].x + regions[i].width,
         y1: 100 - regions[i].y,
         y2: 100 - (regions[i].y + regions[i].height),
-        pageNum: 1,
+        pageNum: this.state.slideNum + 1,
         fileName: 'test',
       }) 
       .then((result) => {
         console.log(result)
       }).catch(err=>console.log(err))
     }
+    this.setState(state => {
+      return {regions: []}
+    })
+  }
+  
+  nextimg = async() => {
+    this.uploadCoords();
+    this.setState(state => {
+      if(state.slideNum < state.imgArray.length - 1){
+        return {slideNum: (state.slideNum + 1), regions: []}
+      }
+    })
+  }
+
+  previmg = async() => {
+    this.uploadCoords();
+    this.setState(state => {
+      if (state.slideNum !== 0) {
+        return {slideNum: (state.slideNum - 1), regions: []}
+      }
+    })
+  }
+
+  importppt = async() => {
+    this.setState(state => {
+      return {imgArray: state.imgArray.concat(['https://i.imgur.com/cZZszWQ.jpg', 'https://i.imgur.com/pBOLLXs.jpg'])}
+    })
+  }
+
+  generate = async() => {
+    http.post('/generate', { 
+      
+    }) 
+    .then((result) => {
+      console.log(result);
+      this.setState(state => {
+        let temp = [];
+        for (let key in result.data.dictionary) {
+          temp.push({word: key, metric: result.data.dictionary[key]});
+        }
+        return {data: temp}
+      })
+
+      console.log(this.state.data);
+    }).catch(err=>console.log(err))
   }
   render() {
+    const columns = [{
+      Header: 'Concept',
+      accessor: 'word' // String-based value accessors!
+    }, {
+      Header: 'Importance',
+      accessor: 'metric',
+    },]
+
     return (
       <div className="App">
-        <header className="App-header">
-          <p>box shit plz</p>
-          <div>
-            <RegionSelect
-              maxRegions={3}
-              regions={this.state.regions}
-              onChange={this.onChange}
-              regionRenderer={this.regionRenderer}
-              style={{ border: '10px solid yellow' }}
-            >
-              <img src='https://i.imgur.com/cZZszWQ.jpg' height='384px' width='512px'></img>
-            </RegionSelect>
-          </div>
-        </header>
-        <div>
-          <button onClick={this.uploadCoords}>
-            <p>MOTHAFUCKA</p>
-          </button>
-        </div>
-        <div>
-          <p>yeet?</p>
+        <div className="App-header">
+          <Tabs>
+            <div label="Slides">
+              <div>
+                <div>
+                  <button style = {{fontSize: "25px", margin: "5px"}} id= 'importbutton' onClick={this.importppt}>IMPORT</button>
+                </div>
+                <div>
+                  <RegionSelect
+                    maxRegions={5}
+                    regions={this.state.regions}
+                    onChange={this.onChange}
+                    regionRenderer={this.regionRenderer}
+                    style={{ border: null }}
+                  >
+                    <img id= "slide" src={this.state.imgArray[this.state.slideNum]} width = "1024px" height = "768px"></img>
+                  </RegionSelect>
+                </div>
+                <div>
+                  <button style = {{fontSize: "25px", margin: "5px"}} onClick= {this.previmg}>PREV</button>
+                  <button style = {{fontSize: "25px", margin: "5px"}} onClick= {this.uploadCoords}>SUBMIT</button>
+                  <button style = {{fontSize: "25px", margin: "5px"}} onClick= {this.nextimg}>NEXT</button>
+                </div>
+              </div>
+            </div>
+
+            <div label="Concepts">
+              <button style = {{fontSize: "25px", margin: "5px"}} onClick= {this.generate}>GENERATE CONCEPTS</button>
+              <div>
+                <ReactTable
+                  data={this.state.data}
+                  columns={columns}
+                  defaultSorted={[
+                    {
+                      id: "metric",
+                      desc: true,
+                    }
+                  ]}
+                  defaultPageSize={8}
+                />
+              </div>
+            </div>
+          </Tabs>
         </div>
       </div>
     );
