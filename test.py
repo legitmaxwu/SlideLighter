93ad6a5e19794ec3aa90aad91850d226df087b27
adamchang2000 <44257083+adamchang2000@users.noreply.github.com>
2018-11-04 05:11:55 -0800

    adjusted img links

---
+++ test.py
@@ -0,0 +1,10 @@
+import ConceptMapGenerator
+import network
+import FirstPDFProcessing
+import ProcessPDF
+
+
+FirstPDFProcessing.CreateJPEGS("test_cal")
+links = ["https://i.imgur.com/hdhj414.jpg", "https://i.imgur.com/eRJ6EVw.jpg", "https://i.imgur.com/unf79Cg.jpg", "https://i.imgur.com/7mQ7TOC.jpg", "https://i.imgur.com/Qir2jMM.jpg", "https://i.imgur.com/ntD8iUs.jpg"]
+ProcessPDF.parse_pdf("../samples/test_cal.pdf")
+network.start(links)
