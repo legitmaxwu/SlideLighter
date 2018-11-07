a78110ad81230a7574e398860ab760b37ee0aa99
adamchang2000 <44257083+adamchang2000@users.noreply.github.com>
2018-11-06 22:14:04 -0800

    ???

--- src/ConceptMapGenerator.py
+++ src/ConceptMapGenerator.py
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-# Imports the Google Cloud client library
-from google.cloud import language
-from google.cloud.language import enums
-from google.cloud.language import types
-
-import os
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "C:/Users/Brandon Lu/Documents/CalHacks/CalHacks-0c0593ef6a43.json"
-
-# Instantiates a client
-client = language.LanguageServiceClient()
-
-# The text to analyze
-text = u'Hello, world!'
-document = types.Document(
-    content=text,
-    type=enums.Document.Type.PLAIN_TEXT)
-
-# Detects the sentiment of the text
-sentiment = client.analyze_sentiment(document=document).document_sentiment
-
-print('Text: {}'.format(text))
-print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
-=======
-
 
 import operator
 import six
@@ -70,4 +45,3 @@ def entities_text(text):
     lst.sort(key = lambda x: -x[1])  
     return lst
     
->>>>>>> 03ff1e70257977361417af337a82c02d44bb9965
